7bde6fb0387a98ab25c152c7c3ae972235e8688d
Arnab Jana <130578687+Arnabjana2003@users.noreply.github.com>
2023-06-06 00:03:11 +0530

    Update main.js

--- main.js
+++ main.js
@@ -7,10 +7,13 @@ btn.addEventListener('click', () =>{
   btn.style.color = "red";
   const inputValue = inputBox.value;
   
+  outputBox.innerText = inputValue;
+  
   let apiLink = `https:api.mymemory.translated.net/get?q=${inputValue}&langpair=eng|ben`;
   fetch(apiLink).then(res => res.json()).then(data =>{
-     const finalData = data.responseData.translatedText;
-    outputBox.innerText = finalData;
+    outputBox.inerText += data; 
+    const finalData = data.responseData.translatedText;
+    outputBox.innerText += finalData;
   })
   
 })
